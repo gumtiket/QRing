f316503e72c2252c42626936ef0642e78d7fa934
gumtiket <gumtiket0303@gmail.com>
2026-09-20 22:40:14 +0900

    Update: nat & igw & routing table

--- infra/terraform/network.tf
+++ infra/terraform/network.tf
@@ -71,3 +71,99 @@ resource "aws_subnet" "db_c" {
     Name = "qring-db-c"
   }
 }
+
+# IGW 생성
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "qring-igw"
+  }
+}
+
+# NAT 게이트웨이를 위한 Elastic IP
+resource "aws_eip" "nat" {
+  domain = "vpc"
+
+  tags = {
+    Name = "qring-nat-eip"
+  }
+}
+
+resource "aws_nat_gateway" "main" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public_a.id
+
+  tags = {
+    Name = "qring-nat"
+  }
+
+  depends_on = [aws_internet_gateway.main]
+}
+
+# 외부 인터넷과 소통하기 위한 라우팅 테이블
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "qring-public-rt"
+  }
+}
+
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_c" {
+  subnet_id      = aws_subnet.public_c.id
+  route_table_id = aws_route_table.public.id
+}
+
+# app 서브넷용 라우팅 테이블
+resource "aws_route_table" "app" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main.id
+  }
+
+  tags = {
+    Name = "qring-app-rt"
+  }
+}
+
+resource "aws_route_table_association" "app_a" {
+  subnet_id      = aws_subnet.app_a.id
+  route_table_id = aws_route_table.app.id
+}
+
+resource "aws_route_table_association" "app_c" {
+  subnet_id      = aws_subnet.app_c.id
+  route_table_id = aws_route_table.app.id
+}
+
+# db 서브넷을 위한 라우팅 테이블(외부랑 연결 안함)
+resource "aws_route_table" "db" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "qring-db-rt"
+  }
+}
+
+resource "aws_route_table_association" "db_a" {
+  subnet_id      = aws_subnet.db_a.id
+  route_table_id = aws_route_table.db.id
+}
+
+resource "aws_route_table_association" "db_c" {
+  subnet_id      = aws_subnet.db_c.id
+  route_table_id = aws_route_table.db.id
+}
