794836d65057db98e5bfc148f93bd98da2103be8
gumtiket <gumtiket0303@gmail.com>
2026-09-21 19:10:40 +0900

    Fix: db ssl

--- server/src/db/client.ts
+++ server/src/db/client.ts
@@ -1,9 +1,21 @@
-import { Pool } from "pg";
+import fs from "fs";
+import { Pool, PoolConfig } from "pg";
 import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "./schema";
 
-const pool = new Pool({
+const caPath = process.env.DATABASE_SSL_CA;
+
+const poolConfig: PoolConfig = {
   connectionString: process.env.DATABASE_URL,
-});
+};
+
+if (caPath) {
+  poolConfig.ssl = {
+    ca: fs.readFileSync(caPath, "utf8"),
+    rejectUnauthorized: true,
+  };
+}
+
+export const pool = new Pool(poolConfig);
 
 export const db = drizzle(pool, { schema });
